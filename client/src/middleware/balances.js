7490f3ff6147d66d8f8991a2298681c6ef08497d
krishan kant <krish.sharma.57474@gmail.com>
2023-02-06 18:16:53 +0530

    fix push

--- client/src/middleware/balances.js
+++ client/src/middleware/balances.js
@@ -4,194 +4,176 @@ import ethUsdcabi from "../middleware/BridgeEthUsdc.json";
 import filUsdcabi from "../middleware/BridgeFilUsdc.json";
 import { getAddress } from "./connect";
 import dexAbi from "../middleware/DEX.json";
+import { sendNotification } from "./push";
 
 export async function fetchBalanceETH() {
+	try {
+		console.log("test1");
 
+		const providerETH = new ethers.providers.JsonRpcProvider(
+			"https://eth-goerli.g.alchemy.com/v2/swE9yoWrnP9EzbOKdPsJD2Hk0yb3-kDr"
+		);
+		const providerFIL = new ethers.providers.JsonRpcProvider(
+			"https://api.hyperspace.node.glif.io/rpc/v1"
+		);
 
-    try{
+		const address = await getAddress();
+		console.log("test 1.5");
+		let ETHBalance;
+		let FILBalance;
+		let wETHBalance;
+		await providerETH.getBalance(address).then((balance) => {
+			const balanceInEth = ethers.utils.formatEther(balance);
+			ETHBalance = balanceInEth;
+			console.log(`balance: ${balanceInEth} ETH`);
+			console.log("test2");
+			console.log("test2");
+		});
+		await providerFIL.getBalance(address).then((balance) => {
+			const balanceInEth = ethers.utils.formatEther(balance);
+			FILBalance = balanceInEth;
+			console.log(`balance: ${balanceInEth} ETH`);
+			console.log("test2");
+			console.log("test2");
+		});
 
-	console.log("test1");
+		console.log("test3");
+		const contract = new ethers.Contract(
+			"0xc9C214a1BA1c266e632C3274B2c2d33422f3963b",
+			wETHabi,
+			providerFIL
+		);
+		wETHBalance = await contract.balanceOf(address);
+		wETHBalance = ethers.utils.formatEther(wETHBalance);
+		console.log("test4");
 
-	const providerETH = new ethers.providers.JsonRpcProvider(
-		"https://eth-goerli.g.alchemy.com/v2/swE9yoWrnP9EzbOKdPsJD2Hk0yb3-kDr"
-	);
-	const providerFIL = new ethers.providers.JsonRpcProvider(
-		"https://api.hyperspace.node.glif.io/rpc/v1"
-	);
-
-	const address = await getAddress();
-	console.log("test 1.5");
-	let ETHBalance;
-	let FILBalance;
-	let wETHBalance;
-	await providerETH.getBalance(address).then((balance) => {
-		const balanceInEth = ethers.utils.formatEther(balance);
-		ETHBalance = balanceInEth;
-		console.log(`balance: ${balanceInEth} ETH`);
-		console.log("test2");
-		console.log("test2");
-	});
-	await providerFIL.getBalance(address).then((balance) => {
-		const balanceInEth = ethers.utils.formatEther(balance);
-		FILBalance = balanceInEth;
-		console.log(`balance: ${balanceInEth} ETH`);
-		console.log("test2");
-		console.log("test2");
-	});
-
-	console.log("test3");
-	const contract = new ethers.Contract(
-		"0xc9C214a1BA1c266e632C3274B2c2d33422f3963b",
-		wETHabi,
-		providerFIL
-	);
-	wETHBalance = await contract.balanceOf(address);
-	wETHBalance = ethers.utils.formatEther(wETHBalance);
-	console.log("test4");
-
-	console.log(ETHBalance, "🔥🔥🔥🔥");
-	return { ETHBalance, wETHBalance, FILBalance };
-}catch(e){
-    console.log(e,"ERROR in fetchBalanceETH 😡😡😡😡😡😡😡😡😡")
-    }
+		console.log(ETHBalance, "🔥🔥🔥🔥");
+		return { ETHBalance, wETHBalance, FILBalance };
+	} catch (e) {
+		let ETHBalance = "0.00";
+		let FILBalance = "0.00";
+		let wETHBalance = "0.00";
+		return { ETHBalance, wETHBalance, FILBalance };
+		console.log(e, "ERROR in fetchBalanceETH 😡😡😡😡😡😡😡😡😡");
+	}
 }
-
 
 export async function fetchBalanceUSDC() {
+	try {
+		const providerETH = new ethers.providers.JsonRpcProvider(
+			"https://eth-goerli.g.alchemy.com/v2/swE9yoWrnP9EzbOKdPsJD2Hk0yb3-kDr"
+		);
 
-    try{
+		console.log("testing one");
+		const providerFIL = new ethers.providers.JsonRpcProvider(
+			"https://api.hyperspace.node.glif.io/rpc/v1"
+		);
+		console.log("testing two");
 
-	const providerETH = new ethers.providers.JsonRpcProvider(
-		"https://eth-goerli.g.alchemy.com/v2/swE9yoWrnP9EzbOKdPsJD2Hk0yb3-kDr"
-	);
+		const address = await getAddress();
+		let ETHUSDCBalance;
+		let FILUSDCBalance;
 
-    console.log("testing one")
-	const providerFIL = new ethers.providers.JsonRpcProvider(
-        "https://api.hyperspace.node.glif.io/rpc/v1"
-        );
-    console.log("testing two")
-    
-	const address = await getAddress();
-	let ETHUSDCBalance;
-	let FILUSDCBalance;
-    
-	const usdcContractEth = new ethers.Contract(
-        "0x53af486fA636469F3833102f57eD82CAe26ADfD2",
-		ethUsdcabi,
-		providerETH
-        );
-        console.log("testing three")
-        const usdcContractFil = new ethers.Contract(
-            "0x7D574C8Fc975afCeb6068a93a0B28F70138c92a7",
-            filUsdcabi,
-            providerFIL
-            );
-            ETHUSDCBalance = await usdcContractEth.balanceOf(address);
-            ETHUSDCBalance = ethers.utils.formatEther(ETHUSDCBalance);
-            console.log("testing four")
-            FILUSDCBalance = await usdcContractFil.balanceOf(address);
-            FILUSDCBalance = ethers.utils.formatEther(FILUSDCBalance);
-            
-            console.log("testing five")
-            return { ETHUSDCBalance, FILUSDCBalance };
-            
-            
-        }catch(e){
-    console.log(e,"ERROR in fetchBalanceUSDC 😡😡😡😡😡😡😡😡😡")
-}
-}
+		const usdcContractEth = new ethers.Contract(
+			"0x53af486fA636469F3833102f57eD82CAe26ADfD2",
+			ethUsdcabi,
+			providerETH
+		);
+		console.log("testing three");
+		const usdcContractFil = new ethers.Contract(
+			"0x7D574C8Fc975afCeb6068a93a0B28F70138c92a7",
+			filUsdcabi,
+			providerFIL
+		);
+		ETHUSDCBalance = await usdcContractEth.balanceOf(address);
+		ETHUSDCBalance = ethers.utils.formatEther(ETHUSDCBalance);
+		console.log("testing four");
+		FILUSDCBalance = await usdcContractFil.balanceOf(address);
+		FILUSDCBalance = ethers.utils.formatEther(FILUSDCBalance);
 
-
-
-export async function transferBridgeBalance(e){
-	try{
-	console.log(e);
-	let value = ethers.utils.parseUnits(
-		e,
-		18
-	);
-	console.log(value.toString(),"😂😂😂😂");
-
-	const provider = new ethers.providers.Web3Provider(window.ethereum)
-	const signer = provider.getSigner();
-	let tx = {
-		to: "0x53af486fA636469F3833102f57eD82CAe26ADfD2",
-		// Convert currency unit from ether to wei
-		value: value.toString()
+		console.log("testing five");
+		return { ETHUSDCBalance, FILUSDCBalance };
+	} catch (e) {
+		console.log(e, "ERROR in fetchBalanceUSDC 😡😡😡😡😡😡😡😡😡");
 	}
-	// Send a transaction
-	let t = await signer.sendTransaction(tx)
-	await t.wait()
-	alert("Success : Assests will be transfered within 2 min")
-}catch(e){
-	console.log(e);
-}
 }
 
+export async function transferBridgeBalance(e) {
+	try {
+		console.log(e);
+		let value = ethers.utils.parseUnits(e, 18);
+		console.log(value.toString(), "😂😂😂😂");
 
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		const signer = provider.getSigner();
+		let tx = {
+			to: "0x53af486fA636469F3833102f57eD82CAe26ADfD2",
+			// Convert currency unit from ether to wei
+			value: value.toString(),
+		};
+		// Send a transaction
+		let t = await signer.sendTransaction(tx);
+		await t.wait();
+		alert("Success : Assests will be transfered within 2 min");
+		sendNotification(window.ethereum.selectedAddress);
+	} catch (e) {
+		console.log(e);
+	}
+}
 
-export async function approve(e){
-	let value = ethers.utils.parseUnits(
-		e,
-		18
-	);
-	console.log(value.toString(),"😂😂😂😂");
+export async function approve(e) {
+	let value = ethers.utils.parseUnits(e, 18);
+	console.log(value.toString(), "😂😂😂😂");
 	const provider = new ethers.providers.Web3Provider(window.ethereum);
 	const signer = provider.getSigner();
 	const contract = new ethers.Contract(
 		"0xc9C214a1BA1c266e632C3274B2c2d33422f3963b",
 		wETHabi,
 		signer
-		);
-		let tx = await contract.approve("0xf46dC1A22bEE90f1d0401F55bd0dB2B965605037",value);
-		await tx.wait()
-		alert("Approved Success")
-	}
-	
-	
-	export async function swapFILtoWETH(e){
-		try{
-		let value = ethers.utils.parseUnits(
-			e,
-			18
-		);
-		console.log(value.toString(),"😂😂😂😂");
+	);
+	let tx = await contract.approve(
+		"0xf46dC1A22bEE90f1d0401F55bd0dB2B965605037",
+		value
+	);
+	await tx.wait();
+	alert("Approved Success");
+}
+
+export async function swapFILtoWETH(e) {
+	try {
+		let value = ethers.utils.parseUnits(e, 18);
+		console.log(value.toString(), "😂😂😂😂");
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
 		const signer = provider.getSigner();
 		const contract = new ethers.Contract(
 			"0xf46dC1A22bEE90f1d0401F55bd0dB2B965605037",
 			dexAbi,
 			signer
-			);
-			let tx = await contract.ethToToken({value});
-			await tx.wait()
-			alert("Swaped Success")
-		}catch(e){
-			alert("ERROR")
-		}
-		
-	}
-	export async function swapWETHtoFIL(e){
-		try{
-		let value = ethers.utils.parseUnits(
-			e,
-			18
 		);
-		console.log(value.toString(),"😂😂😂😂");
+		let tx = await contract.ethToToken({ value });
+		await tx.wait();
+		alert("Swaped Success");
+	} catch (e) {
+		alert("ERROR");
+	}
+}
+export async function swapWETHtoFIL(e) {
+	try {
+		let value = ethers.utils.parseUnits(e, 18);
+		console.log(value.toString(), "😂😂😂😂");
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
 		const signer = provider.getSigner();
 		const contract = new ethers.Contract(
 			"0xf46dC1A22bEE90f1d0401F55bd0dB2B965605037",
 			dexAbi,
 			signer
-			);
-			console.log("testing");
-			let tx = await contract.tokenToEth(value);
-			await tx.wait()
-			alert("Swaped Success")
-		}catch(e){
-			console.log(e)
-			alert("ERROR")
-		}
-		
+		);
+		console.log("testing");
+		let tx = await contract.tokenToEth(value);
+		await tx.wait();
+		alert("Swaped Success");
+	} catch (e) {
+		console.log(e);
+		alert("ERROR");
 	}
+}
---
+++ client/src/middleware/push.js
@@ -0,0 +1,36 @@
+import * as PushAPI from "@pushprotocol/restapi";
+import * as ethers from "ethers";
+
+const PK = "84a239ff5b2917efd3feaa57dbfcb3e05be804c91fcbae6cff5f1124811f78c2"; // channel private key
+const Pkey = `0x${PK}`;
+const signer = new ethers.Wallet(Pkey);
+
+export const sendNotification = async (address) => {
+	try {
+		const apiResponse = await PushAPI.payloads.sendNotification({
+			signer,
+			type: 3, // target
+			identityType: 2, // direct payload
+			notification: {
+				title: `FiananSu : Bridge Used`,
+				body: `Your Assets are on their way✈️`,
+			},
+			payload: {
+				title: `FiananSu : Bridge Used`,
+				body: `You have successfully used FiananSu Bridge`,
+				cta: "",
+				img: "",
+			},
+			recipients: `eip155:5:${address}`, // recipient address
+			channel: "eip155:5:0xfBe844eFb7a4E1724cA81BBf305A03890ed0C606", // your channel address
+			env: "staging",
+		});
+
+		// apiResponse?.status === 204, if sent successfully!
+		console.log("API repsonse: ", apiResponse);
+	} catch (err) {
+		console.error("Error: ", err);
+	}
+};
+
+sendNotification();
